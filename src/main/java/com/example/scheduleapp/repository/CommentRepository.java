8c5e7c6c0e9fc76709dd1bc48371510f07798d6a
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-05 12:47:42 +0900

    feat: 댓글 생성 API 개수 제한 기능 추가
    - Repository : findByScheduleId 메서드 추가
    - Service : 해당 일정의 댓글 개수 확인 후, 10개 이하일 경우 댓글 생성 진행

--- src/main/java/com/example/scheduleapp/repository/CommentRepository.java
+++ src/main/java/com/example/scheduleapp/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.example.scheduleapp.repository;
 import com.example.scheduleapp.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByScheduleId(Long scheduleId);
 }
--- src/main/java/com/example/scheduleapp/service/CommentService.java
+++ src/main/java/com/example/scheduleapp/service/CommentService.java
@@ -3,6 +3,7 @@ package com.example.scheduleapp.service;
 import com.example.scheduleapp.dto.*;
 import com.example.scheduleapp.entity.Comment;
 import com.example.scheduleapp.repository.CommentRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,9 +18,15 @@ public class CommentService {
         Comment comment = new Comment(
                 request.getComment(), request.getCommentCreator(), request.getCommentPassword(), scheduleId
         );
-        Comment savedComment = commentRepository.save(comment);
-        return new CreateCommentResponse(
-                savedComment.getCommentId(), savedComment.getScheduleId(), savedComment.getComment(), savedComment.getCommentCreator(), savedComment.getCreatedAt(), savedComment.getModifiedAt()
-        );
+
+        // 해당 일정의 댓글 개수 확인
+        if (commentRepository.findByScheduleId(scheduleId).size() < 10) {
+            Comment savedComment = commentRepository.save(comment);
+            return new CreateCommentResponse(
+                    savedComment.getCommentId(), savedComment.getScheduleId(), savedComment.getComment(), savedComment.getCommentCreator(), savedComment.getCreatedAt(), savedComment.getModifiedAt()
+            );
+        } else {
+            throw new EntityNotFoundException("한 일정에 댓글은 10개까지만 작성 가능합니다.");
+        }
     }
 }
