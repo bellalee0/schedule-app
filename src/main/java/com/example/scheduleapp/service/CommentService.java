239f6984ee4d420f1b3fdb3f90e516580902b3fa
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-05 10:56:13 +0900

    feat: CommentService, CommentController 클래스 생성

---
+++ src/main/java/com/example/scheduleapp/service/CommentService.java
@@ -0,0 +1,11 @@
+package com.example.scheduleapp.service;
+
+import com.example.scheduleapp.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+}
