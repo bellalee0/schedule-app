93bd63e5b7c02b556563149862a5c0dad20831d9
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-05 19:34:46 +0900

    feat: IncorrectPassword 예외 처리
    - 401 UNAUTHORIZED 반환

---
+++ src/main/java/com/example/scheduleapp/exception/ScheduleExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.scheduleapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ScheduleExceptionHandler {
+    @ExceptionHandler(IncorrectPassword.class)
+    public final ResponseEntity<Map<String, Object>> handleIncorrectPassword(IncorrectPassword ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", new Date());
+        response.put("status", HttpStatus.UNAUTHORIZED.value());
+        response.put("error", HttpStatus.UNAUTHORIZED);
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+}
