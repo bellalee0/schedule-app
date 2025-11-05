c3860652012d59ea2d13bae9d0fb075520c9e12c
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-05 12:07:06 +0900

    feat: 댓글 생성 API 구현
    - Controller : scheduleId를 PathVariable로 받기, request 전달
    - Service : 받은 request 토대로 DB에 저장, responseDTO에 담아서 반환

--- src/main/java/com/example/scheduleapp/controller/CommentController.java
+++ src/main/java/com/example/scheduleapp/controller/CommentController.java
@@ -1,11 +1,25 @@
 package com.example.scheduleapp.controller;
 
+import com.example.scheduleapp.dto.*;
 import com.example.scheduleapp.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @PostMapping("/schedules/{scheduleId}")
+    public ResponseEntity<CreateCommentResponse> createComment(
+            @PathVariable Long scheduleId,
+            @RequestBody CreateCommentRequest request) {
+        CreateCommentResponse result = commentService.createComment(scheduleId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 }
--- src/main/java/com/example/scheduleapp/service/CommentService.java
+++ src/main/java/com/example/scheduleapp/service/CommentService.java
@@ -1,11 +1,25 @@
 package com.example.scheduleapp.service;
 
+import com.example.scheduleapp.dto.*;
+import com.example.scheduleapp.entity.Comment;
 import com.example.scheduleapp.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+
+    @Transactional
+    public CreateCommentResponse createComment(Long scheduleId, CreateCommentRequest request) {
+        Comment comment = new Comment(
+                request.getComment(), request.getCommentCreator(), request.getCommentPassword(), scheduleId
+        );
+        Comment savedComment = commentRepository.save(comment);
+        return new CreateCommentResponse(
+                savedComment.getCommentId(), savedComment.getScheduleId(), savedComment.getComment(), savedComment.getCommentCreator(), savedComment.getCreatedAt(), savedComment.getModifiedAt()
+        );
+    }
 }
