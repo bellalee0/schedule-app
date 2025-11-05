550d79e1d4bfed59ed1259e579ab7867b1287072
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-05 20:27:02 +0900

    feat: existsByCreator 메서드 추가

--- src/main/java/com/example/scheduleapp/repository/ScheduleRepository.java
+++ src/main/java/com/example/scheduleapp/repository/ScheduleRepository.java
@@ -4,4 +4,5 @@ import com.example.scheduleapp.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    boolean existsByCreator(String creator);
 }
