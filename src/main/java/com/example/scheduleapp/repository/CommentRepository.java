86b7be0e82fbde83088c25d7a29a525f94975fc0
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-05 10:53:23 +0900

    feat: CommentRepository 인터페이스 생성
    - JpaRepository 상속

---
+++ src/main/java/com/example/scheduleapp/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.example.scheduleapp.repository;
+
+import com.example.scheduleapp.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
